08468486c35e925963ce53e096ebc1ee14057cdf
Gayatri1199 <56625634+Gayatri1199@users.noreply.github.com>
2022-09-03 17:07:53 +0530

    Referr And Earn

---
+++ assets/js/script.js
@@ -0,0 +1,59 @@
+// var animation = bodymovin.loadAnimation({
+// 	container: document.getElementById('animContainer'),
+// 	renderer: 'canvas',
+// 	loop: true,
+// 	autoplay: true,
+// 	path:'https://assets9.lottiefiles.com/packages/lf20_0etcyzow.json'
+// 	// path:'js/animation-1.json'
+// })
+
+
+var animation = bodymovin.loadAnimation({
+	container: document.getElementById('earn-reward-points'),
+	renderer: 'canvas',
+	loop: true,
+	autoplay: true,
+	path:'https://lottie.host/f8fa013b-76aa-4c96-b7f5-454ba0a9e5a3/jCRagKgnmj.json'
+
+})
+
+
+var animation = bodymovin.loadAnimation({
+	container: document.getElementById('error-image'),
+	renderer: 'canvas',
+	loop: true,
+	autoplay: true,
+	// path:'https://assets10.lottiefiles.com/packages/lf20_jown6jeu.json'
+	path:'https://lottie.host/509fc1cb-0ca1-4644-be1c-c59551545d72/3Etb2Fxrmb.json'
+})
+
+
+
+
+var animation = bodymovin.loadAnimation({
+	container: document.getElementById('redeem-points'),
+	renderer: 'canvas',
+	loop: true,
+	autoplay: true,
+	path:'https://lottie.host/20253254-07c5-47d7-8f6d-cdbee2877764/C5Y5X6Z9Dp.json'
+	
+})
+
+
+var animation = bodymovin.loadAnimation({
+	container: document.getElementById('referral-program'),
+	renderer: 'canvas',
+	loop: true,
+	autoplay: true,
+	path:'https://lottie.host/af7fa189-2b62-4dc8-a9ec-2357dbd54f1c/c7fgQtmmto.json'
+	
+})
+
+var animation = bodymovin.loadAnimation({
+	container: document.getElementById('membership-benefits'),
+	renderer: 'canvas',
+	loop: true,
+	autoplay: true,
+	path:'https://lottie.host/509fc1cb-0ca1-4644-be1c-c59551545d72/3Etb2Fxrmb.json'
+	
+})
